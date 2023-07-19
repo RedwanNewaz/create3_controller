68e4bb25f86e56c8c4b236276ba6c2787d74b5f2
RedwanNewaz <redwan06me@gmail.com>
2023-07-19 09:57:00 -0500

    ready for demo

--- launch/create3_dwa_controller.launch.py
+++ launch/create3_dwa_controller.launch.py
@@ -1,18 +1,9 @@
-import os
-
-from pathlib import Path
-
-from ament_index_python.packages import get_package_share_directory
+import sys
 from launch import LaunchContext, LaunchDescription, SomeSubstitutionsType, Substitution
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-import launch
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -27,6 +18,13 @@ TAG_MAP = {
     'ac31' : 'tag36h11:7',
     'ac32' : 'tag36h11:32'
 }
+
+def getRobotName():
+    robotName = ""
+    nameFilter = lambda x: x.split("=")[-1]
+    if len(sys.argv) > 4:
+        robotName =  nameFilter(sys.argv[4])
+    return robotName
 
 def generate_launch_description():
     current_pkg_dir = get_package_share_directory("create3_controller")
@@ -43,6 +41,8 @@ def generate_launch_description():
         launch_arguments={'namespace': namespace}.items()
         )
     # --ros-args -p control:="/home/roboticslab/colcon_ws/src/create3_controller/config/dwa_param.yaml" -p sensor:=fusion
+
+
     create3_dwa_controller = Node(
         package='create3_controller',
         executable='dwa_controller',
@@ -57,15 +57,15 @@ def generate_launch_description():
         parameters=[
             {'sensor' : 'fusion',
              'control' : os.path.join(current_pkg_dir, 'config/dwa_param.yaml'),
-             'robotTag': TAG_MAP.get(namespace, 'tag36h11:7'),
+             'robotTag': TAG_MAP.get(getRobotName(), 'tag36h11:7'),
              'logOutput' : "/var/tmp"
              }
         ],
         output='screen',
           # disable this line if not using rviz to send goal 
-        remappings=[
-            ('/%s/goal_pose'%namespace, '/goal_pose')
-        ]
+        # remappings=[
+        #     ('/%s/goal_pose'%namespace, '/goal_pose')
+        # ]
     )
 
     create3_rviz = Node(
